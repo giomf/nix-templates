bd2caeabbbf6c1c58ebdbcfe4c549441cfb1adc4
Guillaume Fournier <me@guif.dev>
2026-01-01 23:53:48 +0100

    Add python package to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,26 +1,55 @@
 {
-  description = "Flake templates";
-
+  description = "A simple python flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
   outputs =
-    { ... }:
     {
-      templates = {
-        python = {
-          path = ./python;
-          description = "A simple python template";
-        };
-        rust = {
-          path = ./rust;
-          description = "A simple rust template";
-        };
-        cpp = {
-          path = ./cpp;
-          description = "A simple cpp template";
-        };
-        zola = {
-          path = ./zola;
-          description = "A simple zola template";
-        };
+      self,
+      nixpkgs,
+      pyproject-nix,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      # Variable to easily change Python version
+      pythonVersion = "313"; # 311 for Python 3.11, 312 for Python 3.12, etc.
+      # Use this variable to select the right package set
+      pp = pkgs."python${pythonVersion}Packages";
+      # Loads pyproject.toml into a high-level project representation
+      # Do you notice how this is not tied to any `system` attribute or package sets?
+      # That is because `project` refers to a pure data representation.
+      project = pyproject-nix.lib.project.loadPyproject {
+        # Read & unmarshal pyproject.toml relative to this project root.
+        # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
+        projectRoot = ./.;
+      };
+
+      package =
+        let
+          attrs = project.renderers.buildPythonPackage { python = pp.python; };
+        in
+        pp.python.pkgs.buildPythonPackage (attrs);
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.ruff
+          pkgs.ty
+          pkgs.uv
+          pp.python
+          # Only used for goto definiton/...
+          pp.python-lsp-server
+        ];
+      };
+
+      packages.${system}.default = package;
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${package}/bin/main";
       };
     };
 }
--- python/flake.nix
+++ python/flake.nix
@@ -1,10 +1,17 @@
 {
-  description = "A simple python flake";
+  description = "A simple python flake base on pyproject";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+
   };
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      pyproject-nix,
+    }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -14,6 +21,15 @@
 
       # Use this variable to select the right package set
       pp = pkgs."python${pythonVersion}Packages";
+
+      # Loads pyproject.toml into a high-level project representation
+      # Do you notice how this is not tied to any `system` attribute or package sets?
+      # That is because `project` refers to a pure data representation.
+      project = pyproject-nix.lib.project.loadPyproject {
+        # Read & unmarshal pyproject.toml relative to this project root.
+        # projectRoot is also used to set `src` for renderers such as buildPythonPackage.
+        projectRoot = ./.;
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
@@ -26,5 +42,12 @@
           pp.python-lsp-server
         ];
       };
+      packages.${system}.default =
+        let
+          # Returns an attribute set that can be passed to `buildPythonPackage`.
+          attrs = project.renderers.buildPythonPackage { python = pp.python; };
+        in
+        # Pass attributes to buildPythonPackage.
+        pp.python.pkgs.buildPythonPackage (attrs);
     };
 }
